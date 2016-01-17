f639bec39e8c7596721766055b592913f98fe050
indu05 <rp.indu@gmail.com>
2016-01-18 00:54:41 +0530

    Done
    Soap Ws in Spring Boot

---
+++ src/main/java/com/Application.java
@@ -0,0 +1,17 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
+@SpringBootApplication 
+/*@ComponentScan("com.codenvy")
+@EnableWebMvc*/
+public class Application {
+  
+   public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
